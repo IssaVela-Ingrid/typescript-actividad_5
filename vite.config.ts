24eb6a7cc2742821d9ab70e1efd7c3408e5db7ac
Ingrid Vela <165844332+IssaVela-Ingrid@users.noreply.github.com>
2025-07-05 15:55:11 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-
-
 // vite.config.ts
 import { defineConfig } from 'vite';
 
 export default defineConfig({
- base: '/IssaVela-Ingrid-typescript-actividad_5/',
+  base: '/IssaVela-Ingrid-typescript-actividad_5/', // ¡CRÍTICO! Asegúrate que sea exactamente el nombre de tu repositorio
 });
