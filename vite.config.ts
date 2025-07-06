696a166ee902a6cdba3439181cde0fb36a7d0b01
Ingrid Vela <anabella.iv@gmail.com>
2025-07-05 18:42:10 -0600

    Refresh build 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/IssaVela-Ingrid-typescript-actividad_5/', // ¡CRÍTICO! Asegúrate que sea exactamente el nombre de tu repositorio
+  base: '/typescript-actividad_5/', // ¡CRÍTICO! Asegúrate que sea exactamente el nombre de tu repositorio
 });
